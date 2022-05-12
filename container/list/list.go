74faf0aa292dedcfe372d29ed004d06c928b58c5
ShuwenHe <1201220707@pku.edu.cn>
2022-05-12 09:01:47 +0800

    fix: list

--- container/list/list.go
+++ container/list/list.go
@@ -1,19 +1,19 @@
 package main
 
-import(
-"fmt"
-"container/list"
-) 
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
 	listNew()
 }
 
-func listNew(){
+func listNew() {
 	list := list.New()
 	list.PushBack(1)
 	list.PushBack(2)
-	fmt.Printf("len = %v\n",list.Len())
-	fmt.Printf("first = %#v\n",list.Front())
-	fmt.Printf("first = %#v\n",list.Front().Next())
+	fmt.Printf("len = %v\n", list.Len())
+	fmt.Printf("first = %#v\n", list.Front())
+	fmt.Printf("first = %#v\n", list.Front().Next())
 }
