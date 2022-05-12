c1a4928e614a52459768018e23f87776b0fc355b
ShuwenHe <1201220707@pku.edu.cn>
2022-05-12 09:01:12 +0800

    feat: list

--- container/list/list.go
+++ container/list/list.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import(
+"fmt"
+"container/list"
+) 
 
 func main() {
-	fmt.Println("list")
+	listNew()
+}
+
+func listNew(){
+	list := list.New()
+	list.PushBack(1)
+	list.PushBack(2)
+	fmt.Printf("len = %v\n",list.Len())
+	fmt.Printf("first = %#v\n",list.Front())
+	fmt.Printf("first = %#v\n",list.Front().Next())
 }
