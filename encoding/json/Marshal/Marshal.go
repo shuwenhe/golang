ef59d10bbb5ab4b4d04cba3aea05214f35dd8e6d
Shuwen He <1201220707@pku.edu.cn>
2022-07-15 17:09:27 +0800

    feat: marshal

--- encoding/json/Marshal/Marshal.go
+++ encoding/json/Marshal/Marshal.go
@@ -18,7 +18,7 @@ func m() {
 func m2() {
 	sd := []string{"apple", "peach", "pear"}
 	// sd := []string{"apple"}
-	fmt.Println("sd = ", sd)
+	fmt.Println("sd = ", sd) // sd =  [apple peach pear]
 	sb, _ := json.Marshal(sd)
-	fmt.Println("sb = ", sb)
+	fmt.Println("sb = ", sb) // sb =  [91 34 97 112 112 108 101 34 44 34 112 101 97 99 104 34 44 34 112 101 97 114 34 93]
 }
