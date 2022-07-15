3c8dc1706184d524d8259c52a55025363167e9fc
Shuwen He <1201220707@pku.edu.cn>
2022-07-15 17:00:56 +0800

    feat: marshal

--- encoding/json/Marshal/Marshal.go
+++ encoding/json/Marshal/Marshal.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Marshal")
+	// m()
+	m2()
+}
+
+func m() {
+	b, _ := json.Marshal("gopher")
+	fmt.Println(string(b))
+}
+
+func m2() {
+	// sd := []string{"apple", "peach", "pear"}
+	sd := []string{"apple"}
+	fmt.Println("sd = ", sd)
+	sb, _ := json.Marshal(sd)
+	fmt.Println("sb = ", sb)
 }
